6d41680a17f2e6cebdc423ed45e52d5d9ed0bf37
Tarwine <tarwine@live.com>
2014-05-21 22:13:08 -0400

    Fixed art pipeline structure

--- Source/SeagullGame/BirdCharacter.h
+++ Source/SeagullGame/BirdCharacter.h
@@ -11,6 +11,68 @@
 UCLASS()
 class ABirdCharacter : public ACharacter
 {
+public:
 	GENERATED_UCLASS_BODY()
 
+		/** StaticMesh component that will be the visuals for our flying pawn */
+		UPROPERTY(Category = Mesh, VisibleDefaultsOnly, BlueprintReadOnly)
+		TSubobjectPtr<class UStaticMeshComponent> PlaneMesh;
+
+	/** Spring arm that will offset the camera */
+	UPROPERTY(Category = Camera, VisibleDefaultsOnly, BlueprintReadOnly)
+		TSubobjectPtr<class USpringArmComponent> SpringArm;
+
+	/** Camera component that will be our viewpoint */
+	UPROPERTY(Category = Camera, VisibleDefaultsOnly, BlueprintReadOnly)
+		TSubobjectPtr<class UCameraComponent> Camera;
+
+	// Begin AActor overrides
+	virtual void Tick(float DeltaSeconds) OVERRIDE;
+	virtual void ReceiveHit(class UPrimitiveComponent* MyComp, class AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) OVERRIDE;
+	// End AActor overrides
+
+protected:
+
+	// Begin APawn overrides
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) OVERRIDE; // Allows binding actions/axes to functions
+	// End APawn overrides
+
+	/** Bound to the vertical axis */
+	void ThrustInput(float Val);
+
+	/** Bound to the horizontal axis */
+	void MoveUpInput(float Val);
+
+	/** */
+	void MoveRightInput(float Val);
+
+private:
+
+	/** How quickly forward speed changes */
+	UPROPERTY(Category = Plane, EditAnywhere)
+		float Acceleration;
+
+	/** How quickly pawn can steer */
+	UPROPERTY(Category = Plane, EditAnywhere)
+		float TurnSpeed;
+
+	/** MAx forward speed */
+	UPROPERTY(Category = Pitch, EditAnywhere)
+		float MaxSpeed;
+
+	/** Min forward speed */
+	UPROPERTY(Category = Yaw, EditAnywhere)
+		float MinSpeed;
+
+	/** Current forward speed */
+	float CurrentForwardSpeed;
+
+	/** Current yaw speed */
+	float CurrentYawSpeed;
+
+	/** Current pitch speed */
+	float CurrentPitchSpeed;
+
+	/** Current roll speed */
+	float CurrentRollSpeed;
 };
