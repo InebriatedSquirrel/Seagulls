80787c98d3ba436877a90a6d4324283b82674a2e
Tarwine <tarwine@live.com>
2014-05-30 14:09:29 -0400

    Functional gliding before code cleanup

--- Source/SeagullGame/BirdCharacter.cpp
+++ Source/SeagullGame/BirdCharacter.cpp
@@ -34,9 +34,13 @@ ABirdCharacter::ABirdCharacter(const class FPostConstructInitializeProperties& P
 
 	// Set handling parameters
 	TurnSpeed = 50.f;
-	gliding = false;
+	Gliding = false;
+	GlidingUnlocked = true;
+	FlyingGravityStrength = 0.2f;
 	MaxVerticalFlapVelocity = 500.0f;
-	flapStrength = 350.0f;
+	VertFlapStrength = 350.0f;
+	MaxGlideForce = 10.0f;
+	GlideDragAmount = 0.1f;
 }
 
 // Frame loop
@@ -49,9 +53,9 @@ void ABirdCharacter::Tick(float DeltaSeconds)
 		BothTimer += DeltaSeconds;
 		if (BothTimer < 0.3f){
 			// Launch the player upwards at a moderate strength
-			const FVector LaunchForce = FVector(0.f, 0.f, 350.0f);
+			const FVector LaunchForce = FVector(0.f, 0.f, VertFlapStrength);
 			// Set a max limit on vertical velocity
-			if (GetVelocity().Z < 500.0f){
+			if (GetVelocity().Z < MaxVerticalFlapVelocity){
 				LaunchCharacter(LaunchForce, false, false);
 			}
 		}
@@ -63,13 +67,6 @@ void ABirdCharacter::Tick(float DeltaSeconds)
 		if (RightTimer < 0.15f){
 			// Rotate the bird clockwise
 			RotateBird(1.0f);
-			// Launch the player upwards at a weak strength
-			const FVector LaunchForce = FVector(0.f, 0.f, 50.0f);
-			// Set a max limit on vertical velocity
-			if (GetVelocity().Z < 100.0f){
-
-				LaunchCharacter(LaunchForce, false, false);
-			}
 		}
 	}
 	else if (LeftFlapped)
@@ -79,12 +76,6 @@ void ABirdCharacter::Tick(float DeltaSeconds)
 		if (LeftTimer < 0.15f){
 			// Rotate the bird counter-clockwise
 			RotateBird(-1.0f);
-			// Launch the player upwards at a weak strength
-			const FVector LaunchForce = FVector(0.f, 0.f, 50.0f);
-			// Set a max limit on vertical velocity
-			if (GetVelocity().Z < 100.0f){
-				LaunchCharacter(LaunchForce, false, false);
-			}
 		}
 	}
 
@@ -100,26 +91,35 @@ void ABirdCharacter::Tick(float DeltaSeconds)
 	// Smoothly interpolate to target yaw speed
 	CurrentYawSpeed = FMath::FInterpTo(CurrentYawSpeed, 0.0f, GetWorld()->GetDeltaSeconds(), 2.0f);
 
-//	ForwardFlapForce = FMath::FInterpTo(ForwardFlapForce, 0.0f, GetWorld()->GetDeltaSeconds(), 5.0f);
-	if (ForwardFlapForce > 0.0f){
-		ForwardFlapForce -= 0.005f;
+	if (LatFlapForce >= 0.0f ){
+		LatFlapForce -= GlideDragAmount * Controller->GetControlRotation().Vector().Z;
 	}
-	else if (ForwardFlapForce < 0.0f){
-		ForwardFlapForce = 0.0f;
+	if (LatFlapForce >= 0.0f && !ForwardPressed){
+		LatFlapForce = 0.0f;
+	}
+	if (LatFlapForce < 0.0f || CharacterMovement->MovementMode == MOVE_Walking){
+		LatFlapForce = 0.0f;
+	}
+	else if (LatFlapForce > MaxGlideForce){
+		LatFlapForce = MaxGlideForce;
+	}
+	if (ForwardPressed){
+		const FVector Direction = Controller->GetControlRotation().Vector();
+		AddMovementInput(Direction, LatFlapForce);
 	}
 
-	if (gliding){
+	if (Gliding){
 		const FVector Direction = Controller->GetControlRotation().Vector();
-		//AddMovementInput(Direction, 0.2f);
-		AddMovementInput(FVector(0.0f,0.0f,-1.0f), 0.2f);
+		
+		AddMovementInput(FVector(0.0f, 0.0f, -1.0f), FlyingGravityStrength);
 		if (CharacterMovement->MovementMode != MOVE_Flying){
 			CharacterMovement->SetMovementMode(MOVE_Flying);
 		}
 		
 	}
-
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::SanitizeFloat(ForwardFlapForce));
-
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, CharacterMovement->GetMovementName());
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::SanitizeFloat(LatFlapForce));
+	
 	// Call any parent class Tick implementation
 	Super::Tick(DeltaSeconds);
 }
@@ -127,7 +127,10 @@ void ABirdCharacter::Tick(float DeltaSeconds)
 void ABirdCharacter::ReceiveHit(class UPrimitiveComponent* MyComp, class AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
 {
 	Super::ReceiveHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
-
+	// If the bird touches an object with the ground tag, mark the player as walking
+	if (Other->ActorHasTag("Ground")){
+		CharacterMovement->SetMovementMode(MOVE_Walking);
+	}
 }
 
 void ABirdCharacter::SetupPlayerInputComponent(class UInputComponent* InputComponent)
@@ -136,6 +139,8 @@ void ABirdCharacter::SetupPlayerInputComponent(class UInputComponent* InputCompo
 
 	InputComponent->BindAxis("Forward", this, &ABirdCharacter::ThrustInput);
 	
+	InputComponent->BindAxis("Strafe", this, &ABirdCharacter::Strafe);
+
 	InputComponent->BindAxis("Lateral", this, &ABirdCharacter::AddControllerYawInput);
 	InputComponent->BindAxis("Vertical", this, &ABirdCharacter::AddControllerPitchInput);
 	InputComponent->BindAction("Flap", IE_Pressed, this, &ABirdCharacter::Flap);
@@ -146,40 +151,43 @@ void ABirdCharacter::SetupPlayerInputComponent(class UInputComponent* InputCompo
 
 void ABirdCharacter::Flap(){
 	// Launch the player upwards at a weak strength
-	const FVector LaunchForce = FVector(0.f, 0.f, 250.0f);
+	const FVector LaunchForce = FVector(0.f, 0.f, VertFlapStrength);
 	const FVector Direction = Controller->GetControlRotation().Vector();
 	// Set a max limit on vertical velocity
-	if (GetVelocity().Z < 100.0f){
+	if (GetVelocity().Z < MaxVerticalFlapVelocity){
 		LaunchCharacter(LaunchForce, false, false);
-		ForwardFlapForce += 0.5f;
+		if (LatFlapForce < MaxGlideForce){
+			LatFlapForce += 0.5f;
+		}
 	}
-
-	gliding = true;
+	if (GlidingUnlocked){
+		Gliding = true;
+	}
 }
 void ABirdCharacter::StopGlide(){
-	gliding = false;
+	Gliding = false;
 	CharacterMovement->SetMovementMode(MOVE_Falling);
 }
 void ABirdCharacter::ThrustInput(float Val)
 {
-	ForwardPressed = true;
-
-	// find out which way is forward
-	FRotator Rotation = Controller->GetControlRotation();
-	if (!gliding){
-		// add movement in that direction
-		//const FVector Direction = FRotationMatrix(Rotation).GetScaledAxis(EAxis::X, EAxis::Y, EAxis::Z);
-		const FVector Direction = Controller->GetControlRotation().Vector();
-		//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, Direction.ToString());
-		AddMovementInput(Direction, Val);
+	// If holding forward, allow increase in speed
+	if (Val > 0.0f){
+		ForwardPressed = true;
 	}
+	// If holding reverse, decrease speed
+	else if (Val < 0.0f){
+		ForwardPressed = false;
+		LatFlapForce -= 0.05f;
+	}
+	// Toggle no input detected
 	else{
-		const FVector Direction = Controller->GetControlRotation().Vector();
-		AddMovementInput(Direction, ForwardFlapForce);
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, "Flying Forward");
+		ForwardPressed = false;
 	}
 }
-
+void ABirdCharacter::Strafe(float Val){
+	const FVector LatDirection = FRotationMatrix(GetActorRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(LatDirection, (Val * LatFlapForce));
+}
 void ABirdCharacter::OnRightFlap(float Val)
 {
 	if (Val == -0.5f)
@@ -214,8 +222,5 @@ void ABirdCharacter::RotateBird(float Val)
 
 	// Smoothly interpolate to target yaw speed
 	CurrentYawSpeed = FMath::FInterpTo(CurrentYawSpeed, TargetYawSpeed, GetWorld()->GetDeltaSeconds(), 2.f);
-
-	// find out which way is forward
-	FRotator Rotation = Controller->GetControlRotation();
 }
 
--- Source/SeagullGame/BirdCharacter.h
+++ Source/SeagullGame/BirdCharacter.h
@@ -27,18 +27,42 @@ public:
 	virtual void ReceiveHit(class UPrimitiveComponent* MyComp, class AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) OVERRIDE;
 	// End AActor overrides
 
-	/** How quickly pawn can steer */
+	
 	UPROPERTY(Category = Bird, EditAnywhere, BlueprintReadWrite)
-	float TurnSpeed;
+		// How fast the bird rotates
+		float TurnSpeed;
 
 	UPROPERTY(Category = Bird, EditAnywhere, BlueprintReadWrite)
-	float flapStrength;
+		// Upwards force when flapping
+		float VertFlapStrength;
 
 	UPROPERTY(Category = Bird, EditAnywhere, BlueprintReadWrite)
-	float MaxVerticalFlapVelocity;
+		// Maximum velocity when travelling upwards
+		float MaxVerticalFlapVelocity;
+
+	UPROPERTY(Category = Bird, EditAnywhere, BlueprintReadWrite)
+		// Has the player unlocked the glide ability
+		bool GlidingUnlocked;
+
+	UPROPERTY(Category = Bird, EditAnywhere, BlueprintReadWrite)
+		// The downwards force placed on the player
+		float FlyingGravityStrength;
+
+	UPROPERTY(Category = Bird, EditAnywhere, BlueprintReadWrite)
+		// The max force when gliding - influenced by LatFlapForce and GlideDragAmount
+		float MaxGlideForce;
+
+	UPROPERTY(Category = Bird, EditAnywhere, BlueprintReadWrite)
+		// The drag imparted against LatFlapForce when gliding - also increases dive speed
+		float GlideDragAmount;
 
 	UPROPERTY(Category = Bird, BlueprintReadOnly)
-	bool gliding;
+		// Is the player currently gliding
+		bool Gliding;
+
+	UPROPERTY(Category = Bird, BlueprintReadOnly)
+		// The force that flapping adds to forward/sideways movement
+		float LatFlapForce;
 
 protected:
 
@@ -46,47 +70,49 @@ protected:
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) OVERRIDE; // Allows binding actions/axes to functions
 	// End APawn overrides
 
-	/** Bound to the vertical axis */
+	// Toggle forward movement
 	void ThrustInput(float Val);
 
-	/** Event for Rotations*/
+	// Event for Rotations
 	void OnRightFlap(float Val);
 
-	/** Rotates the bird around it's axis*/
+	/// Rotates the bird around it's axis
 	void RotateBird(float Val);
 
-	/**  */
+	// Called when the main flap button is pressed
 	void Flap();
 
-	/**  */
+	// Function which stops gliding when glide button is released
 	void StopGlide();
+
+	// Strafes the player left or right when gliding
+	void Strafe(float Val);
 
 private:
 
-	
-
-	/** Current yaw speed */
+	// Current yaw speed 
 	float CurrentYawSpeed;
 
-	/** Current pitch speed */
+	// Current pitch speed
 	float CurrentPitchSpeed;
 
-	/** Current roll speed */
+	// Current roll speed 
 	float CurrentRollSpeed;
 
-	/** Booleans for input tracking*/
+	// Was the right wing just flapped
 	bool RightFlapped;
-
+	// Was the right wing just flapped
 	bool LeftFlapped;
-
+	// Were both wings just flapped
 	bool ForwardPressed;
 
 	/** Timers for wing flap spacing*/
+	// Cooldown for right wing
 	float RightTimer;
-
+	// Cooldown for right wing
 	float LeftTimer;
-
+	// Cooldown for both wings
 	float BothTimer;
 
-	float ForwardFlapForce;
+	
 };
