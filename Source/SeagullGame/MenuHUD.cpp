7877e0c8fdf2dddc59c69006c102a25377edba69
Tarwine <tarwine@live.com>
2014-07-02 15:51:31 -0400

    Attemped to add slate methods

--- Source/SeagullGame/MenuHUD.cpp
+++ Source/SeagullGame/MenuHUD.cpp
@@ -15,4 +15,8 @@ void AMenuHUD::ReceiveDrawHUD(int32 SizeX, int32 SizeY)
 	DrawRect(FLinearColor::Black, 100.f, 100.f, 50.f, 50.f);
 }
 
+void AMainMenuHUD::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+}
 
