0b230bfb5c70f7909c8d5edd2e5f77073b83e280
Tarwine <tarwine@live.com>
2014-07-11 03:01:27 -0400

    Fullscreen checkbox and config load

--- Source/SeagullGame/UI/Widgets/GraphicsWidget.cpp
+++ Source/SeagullGame/UI/Widgets/GraphicsWidget.cpp
@@ -11,13 +11,20 @@ void SGraphicsWidget::Construct(const FArguments& args)
 
 	MenuStyle = &FMenuStyles::Get().GetWidgetStyle<FGlobalStyle>("GlobalMenuStyle");
 
+	Resolutions.Add(MakeShareable(new FString("1920x1200")));
 	Resolutions.Add(MakeShareable(new FString("1920x1080")));
+	Resolutions.Add(MakeShareable(new FString("1680x1050")));
+	Resolutions.Add(MakeShareable(new FString("1600x900")));
 	Resolutions.Add(MakeShareable(new FString("1280x720")));
 
-	SelectedRes = *Resolutions[0];
+	SelectedRes = Resolutions[4];
 
 	//GEngine->AddOnScreenDebugMessage(0, 1.f, FColor::Red, *Resolutions[0] );
 	//GEngine->AddOnScreenDebugMessage(0, 1.f, FColor::Red, *Resolutions[1] );
+
+
+	//Use this to grab data
+	//UEngine::LoadConfig();
 
     ChildSlot
         [
@@ -27,32 +34,50 @@ void SGraphicsWidget::Construct(const FArguments& args)
                 .VAlign(VAlign_Center)
                 [
 					SNew(SHorizontalBox)
-
+					
 					+ SHorizontalBox::Slot().Padding(10.0f)
 					[
 						SNew(SVerticalBox)
 
 						+ SVerticalBox::Slot().Padding(10.0f)
 						[
-							SNew(SButton)
-								.HAlign(HAlign_Center)
-								.VAlign(VAlign_Center)
-								.Text(FText::FromString("Back"))
-								.OnClicked(this, &SGraphicsWidget::BackClicked)
-								.Cursor(EMouseCursor::Hand)
-						]
-						+ SVerticalBox::Slot().Padding(10.0f)
-						[
 							SAssignNew(this->ResolutionButton, SComboBox<TSharedPtr<FString>>)
 							.OptionsSource(&Resolutions)
 							.OnGenerateWidget(this, &SGraphicsWidget::OnGenerateWidget)
 							.OnSelectionChanged(this, &SGraphicsWidget::OnSelectedRes)
-							.InitiallySelectedItem(Resolutions[0])
+							.InitiallySelectedItem(SelectedRes)
 							.Cursor(EMouseCursor::Hand)
 							.Content()
 							[
-								SNew(STextBlock).Text(SelectedRes)
+								SAssignNew(this->ResDisplay, STextBlock)
+									.Text(*SelectedRes)
 							]
+						]
+						+ SVerticalBox::Slot().Padding(10.0f)
+						[
+							SNew(SHorizontalBox)
+
+							+ SHorizontalBox::Slot().HAlign(HAlign_Left).AutoWidth()
+							[
+								SNew(STextBlock)
+								.Text(FString("FullScreen"))
+								.ColorAndOpacity(FLinearColor::Black)
+							]
+							+ SHorizontalBox::Slot().HAlign(HAlign_Right)
+							[
+								SNew(SCheckBox)
+								.OnCheckStateChanged(this, &SGraphicsWidget::FullScreenClicked)
+								.Cursor(EMouseCursor::Hand)
+							]
+						]
+						+ SVerticalBox::Slot().Padding(10.0f)
+						[
+							SNew(SButton)
+							.HAlign(HAlign_Center)
+							.VAlign(VAlign_Center)
+							.Text(FText::FromString("Back"))
+							.OnClicked(this, &SGraphicsWidget::BackClicked)
+							.Cursor(EMouseCursor::Hand)
 						]
 					]
                 ]
@@ -66,15 +91,31 @@ FReply SGraphicsWidget::BackClicked()
 	return FReply::Handled();
 }
 
+void SGraphicsWidget::FullScreenClicked(const ESlateCheckBoxState::Type NewCheckedState)
+{
+	if (NewCheckedState == ESlateCheckBoxState::Checked)
+	{
+		GEngine->GameViewport->ConsoleCommand("SETRES " + *SelectedRes + "f");
+	}
+	else if (NewCheckedState == ESlateCheckBoxState::Unchecked)
+	{
+		GEngine->GameViewport->ConsoleCommand("SETRES " + *SelectedRes + "w");
+	}
+}
+
 TSharedRef<SWidget> SGraphicsWidget::OnGenerateWidget(TSharedPtr<FString> Item)
 {
 	return SNew(STextBlock).Text(*Item);
+
 }
 
 void SGraphicsWidget::OnSelectedRes(TSharedPtr<FString> Item, ESelectInfo::Type SelectInfo)
 {
-	SelectedRes = *Item;
+	SelectedRes = Item;
 	ResolutionButton->SetSelectedItem(Item);
+	ResDisplay->SetText(*SelectedRes);
 	ResolutionButton->RefreshOptions();
+
+	GEngine->GameViewport->ConsoleCommand("SETRES " + *Item);
 	//ResolutionButton->SetMenuContent(SNew(STextBlock).Text(*Item));
 }
--- Source/SeagullGame/UI/Widgets/GraphicsWidget.h
+++ Source/SeagullGame/UI/Widgets/GraphicsWidget.h
@@ -20,21 +20,22 @@ public:
 	//Constructs and lays out the Main Menu UI Widget.
     void Construct(const FArguments& args);
 
-	FString SelectedRes;
+	TSharedPtr<FString> SelectedRes;
  
 private:
 
 	const struct FGlobalStyle* MenuStyle;
 
 	TArray<TSharedPtr<FString>> Resolutions;
-
 	TSharedPtr<SComboBox<TSharedPtr<FString>>> ResolutionButton;
+	void SGraphicsWidget::OnSelectedRes(TSharedPtr<FString> Item, ESelectInfo::Type SelectInfo);
+	TSharedPtr<class STextBlock> ResDisplay;
+
+	void FullScreenClicked(const ESlateCheckBoxState::Type NewCheckedState);
 
 	FReply BackClicked();
 
 	TSharedRef<SWidget> SGraphicsWidget::OnGenerateWidget(TSharedPtr<FString> Item);
-
-	void SGraphicsWidget::OnSelectedRes(TSharedPtr<FString> Item, ESelectInfo::Type SelectInfo);
 
     //Stores a weak reference to the HUD controlling this class.
     TWeakObjectPtr<class AMenuHUD> MenuHUD;
